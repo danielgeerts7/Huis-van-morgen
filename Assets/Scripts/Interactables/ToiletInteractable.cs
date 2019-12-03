e7bee2d2ae9d2210a5ab77ac4f1f95be1be51ab0
Leon Smit <leon_94@live.nl>
2019-12-03 15:08:03 +0100

    Bam house

---
+++ Assets/Scripts/Interactables/ToiletInteractable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletInteractable : Interactable
+{
+    public override bool isActive()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnActivate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnDeselect()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnSelect()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
