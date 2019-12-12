deb00db622a7a5263d535b7e2dbbe32e31570eab
Leon Smit <leon_94@live.nl>
2019-12-12 14:37:33 +0100

    Stairs teleporter

---
+++ Assets/TeleportInteractable.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportInteractable : Interactable
+{
+    public GameObject spot1;
+    public GameObject spot2;
+
+    private GameObject player;
+
+    public override bool isActive()
+    {
+        return false;
+    }
+
+    public override void OnActivate()
+    {
+        float dist1 = Vector3.Distance(player.transform.position, spot1.transform.position);
+        float dist2 = Vector3.Distance(player.transform.position, spot2.transform.position);
+
+        if (dist1 < dist2)
+        {
+            TeleportTo(spot2);
+        }
+        else
+        {
+            TeleportTo(spot1);
+        }
+    }
+
+    private void TeleportTo(GameObject go)
+    {
+        player.transform.position = go.transform.position;
+
+
+        //TODO: Fix rotation
+        player.transform.rotation = Quaternion.Euler(go.transform.rotation.eulerAngles);
+    }
+
+    public override void OnDeselect()
+    {
+        
+    }
+
+    public override void OnSelect()
+    {
+        
+    }
+
+    public override void OnStart()
+    {
+        player = FindObjectOfType<DebugPlayerController>().gameObject;
+
+        if (!player)
+            player = FindObjectOfType<OVRPlayerController>().gameObject;
+    }
+
+    public override void OnUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            OnActivate();
+        }
+    }
+}
