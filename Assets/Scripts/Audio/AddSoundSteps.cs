0648fcf2860e3f81cd00b0d2404832ce1a7865dc
Florian Molenaars <f.molenaars@hotmail.com>
2020-01-07 12:59:54 +0100

    Added ambulance sound and sound on step script

---
+++ Assets/Scripts/Audio/AddSoundSteps.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSoundSteps : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Scenario scenario;
+    public string nameSong;
+    private Step step;
+
+    void Start()
+    {
+        step = GetComponent<Step>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (step.getState() == State.RUNNING)
+            GameObject.FindObjectOfType<AudioManager>().Play(nameSong);
+    }
+}
